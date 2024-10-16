6912f75d857e29f68ed71c3f8ec9423f5dd5f933
Vitaly Lebedkov <lebedkov.2000@mail.ru>
2024-10-16 19:57:29 +0700

    v 1.1.0

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-const galleryItem = document.querySelectorAll('.gallery__item')
-for (let i = 0; i < galleryItem.length; i++) {
-    galleryItem[i].onclick = function () {
-        galleryItem[i].classList.toggle('modal')
-        if (galleryItem[i].classList.contains('modal')) {
-            document.querySelector('.body').classList.add('no-scroll')
-        }
-        if (!galleryItem[i].classList.contains('modal')) {
-            document.querySelector('.body').classList.remove('no-scroll')
+// Создаём посты при помощи массива
+const arrayOfPictures = []
+for (let i = 0; i < 6; i++) {
+    const image = new Image()
+    image.src = `./pictures/section-posts-${i + 1}.png`
+    arrayOfPictures.unshift(image)
+};
+
+function showPictures() {
+    arrayOfPictures.map(el => document.querySelector('.gallery').insertAdjacentElement('afterbegin', el));
+    Array.from(document.querySelector('.gallery').children).forEach(el => {
+        el.classList.add('gallery__item')
+    });
+}
+
+showPictures()
+
+
+// Клик по фотографии (посту)
+for (let element of arrayOfPictures) {
+    element.onclick = function (click) {
+        document.querySelector('.modal').classList.remove('visually-hidden')
+        document.querySelector('.modal-content').insertAdjacentElement('afterbegin', element)
+        document.querySelector('.modal-content').firstChild.classList.add('try')
+
+        if (!document.querySelector('.modal').classList.contains('visually-hidden')) {
+            document.getElementById('body').style.overflow = 'hidden'
         }
     }
 }
 
+document.querySelector('.back').onclick = function(click) {
+    document.querySelector('.modal').classList.add('visually-hidden')
+    document.querySelector('.modal-content').firstChild.classList.remove('try')
+
+    if (document.querySelector('.modal').classList.contains('visually-hidden')) {
+        document.getElementById('body').style.overflow = 'visible'
+    }
+    
+    showPictures()
+}
+
+
+
+//Кнопка FOLLOW
 document.querySelector('.account__button').onclick = function () {
     if (document.querySelector('.account__button').classList.contains('account__button')) {
         document.querySelector('.account__button').classList.toggle('account__button-active')
         document.querySelector('.account__button').innerHTML = 'Follow'
 
-        if ( document.querySelector('.account__button').classList.contains('account__button-active')) {
+        if (document.querySelector('.account__button').classList.contains('account__button-active')) {
             document.querySelector('.account__button').innerHTML = 'Unfollow'
         }
     }
-}
+};
+
+
+
+
+
